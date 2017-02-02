fff85a8e62384c9d7460443294da49213cc9af57
AnilAmjed <anilamjed@gmail.com>
2017-02-02 11:40:48 -0800

    Assignment 1
    Hello World

---
+++ Assignment1.java
@@ -0,0 +1,11 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) 
+	{
+		//hello world program
+		//system.out.println "command is use for print on screen"
+System.out.println("Hello World");
+	}
+
+}
